af7c7641ceafe5730f2129d15361e0e7ad5e3811
kibiddd <kibiddd@gmail.com>
2025-03-07 06:08:31 -0500

    Update model.py

--- model.py
+++ model.py
@@ -5,12 +5,12 @@ import main
 def domain_analysis():
 	pipe = pipeline("text-generation", model="microsoft/Phi-3.5-mini-instruct", trust_remote_code=True)
 	task = """Based on the URL and the domain registration information, how likely is the website fraudulent?
-Specifically, look at (1) Is the registration recent and/or expiring in less than a year? 
+Specifically, look at (1) Is the registration recent and/or expiring in less than a year? It is 2025-03-07 today.
 (2) Is the contact information redacted or partial?
 (3) Could the domain name be used to create a false sense of legitimacy?
-(4) Other suspicious factors.
+(4) Other suspicious factors. Answer N/A if none.
 Based on the answer to the above questions, give your final verdict on a scale of 1 to 10, with 10 being most likely.
-Output should be in json format: {"answer1": answer1, "answer2": answer2, "answer3": answer3, "answer4": answer4, "verdict": verdict(1-10)}"""
+Output should be a strict json format: {"answer1": answer1, "answer2": answer2, "answer3": answer3, "answer4": answer4, "verdict": verdict(1-10)} without any other comment."""
 	url = "https://cyberfraudlawyers.com/"
 	info = main.whois_info(url)
 	task = task + "\nURL=" + url + "\nInfo=" + str(info)
