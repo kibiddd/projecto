86aa57671a30f2e243e09f41a2258bf9e3ffeca6
kibiddd <kibiddd@gmail.com>
2025-03-12 05:16:56 -0400

    Update domain.py

--- domain.py
+++ domain.py
@@ -3,7 +3,16 @@ from transformers import pipeline
 from datetime import datetime
 import whois
 
-def domain_analysis(pipe, url, info):
+# function that checks if registration info is invalid
+def check_info(domain_info):
+	if domain_info.startswith("No match") or domain_info.startswith("Streamline cancelled"):
+		return True
+	return False
+
+def domain_analysis(pipe, url, domain_info):
+	if check_info(info):
+		print("Invalid info")
+		return "Invalid info"
 	# pipe = pipeline("text-generation", model="microsoft/Phi-4-mini-instruct", trust_remote_code=True)
 	date = datetime.today().strftime('%Y-%m-%d')
 	task = f"""Analyze this domain registration data and provide an overall scam risk score from 0-10 (10 = highest risk). Consider these factors:
@@ -12,7 +21,7 @@ def domain_analysis(pipe, url, info):
 2.  Expiry Date: Domains expiring soon (within 1 year) may be riskier. 
 3.  Contact Information: Redacted, incomplete, or suspicious WHOIS details increase risk.
 4.  Other Factors: Any other suspicious registration information
-	If no other information than URL is given, give a score of 0 and N/A as explanation."""
+	If no other information than URL is given, give a score of 0 and N/A as reasoning."""
 
 	format_specification = """Output in strict JSON format:
 	{"registration":"reasoning","expiry":"reasoning","contact":"reasoning","other":"reasoning","score":1-10}"""
@@ -20,7 +29,7 @@ def domain_analysis(pipe, url, info):
 	task = task + "\n" + format_specification
 
 	# info = whois.whois_info(url)
-	task = task + "\nURL=" + url + "\nInfo=" + str(info)
+	task = task + "\nURL=" + url + "\nInfo=" + str(domain_info)
 
 	messages = [
 		{
@@ -38,8 +47,8 @@ def domain_analysis(pipe, url, info):
 if __name__ == "__main__":
 	pipe = pipeline("text-generation", model="microsoft/Phi-4-mini-instruct", trust_remote_code=True)
 	#url = 'http://yummy-rhinestone-button.glitch.me/hover.html'
-	url = 'https://scraper.tech/screenshots/4080609971.png'
-	with open('legit-250311/414.txt', 'r') as f:
+	url = 'https://www.thekingsmeadow.net/FOSTER-CRAWFORD.htm'
+	with open('legit-250311/31.txt', 'r') as f:
 		info = f.read()
-	domain_analysis(pipe, url, info)
+	domain_analysis(pipe=None, url=url, domain_info=info)
 
